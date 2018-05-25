5ab40f3e581832f4cf155212b520f71c261c83f2
azzamalthagafi <azzam.thagafi@gmail.com>
2018-05-25 15:04:15 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import CircleArea from './CircleArea';
+import BuffonNeedle from './BuffonNeedle';
+
+import './App.css';
+import $ from "jquery";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { simulationID: 'C' };
+  }
+
+  renderSimulation() {
+    switch (this.state.simulationID) {
+      case 'C':
+        return <CircleArea width={500} height={500}/>;
+
+      case 'BN':
+        return <BuffonNeedle />
+
+      default:
+        return <CircleArea width={500} height={500}/>;
+    }
+  }
+
+  handleClick = (simulationID) => {
+    $(".btn-group > .btn").removeClass("active");
+    $('#' + simulationID).addClass("active");
+    this.setState({ simulationID });
+  }
+
+  renderHeader() {
+    return  (
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <a className="navbar-brand">
+          <img src={logo} width="30" height="30" className="d-inline-block align-top mr-2" alt=""/>
+          Estimating Pi
+        </a>
+        <div>
+          <span className="navbar-text mr-2">
+            Estimation method:
+          </span>
+          <div className="btn-group" id="SimOptions" role="group" aria-label="Estimation Methods">
+            <button type="button" id="C" className="btn btn-secondary active" onClick={() => this.handleClick('C')}>Circle Area</button>
+            <button type="button" id="BN" className="btn btn-secondary" onClick={() => this.handleClick('BN')}>Buffon's needle</button>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderHeader()}
+        <div className="container mt-2">
+          {this.renderSimulation()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/CircleArea.js
@@ -0,0 +1,184 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { select } from 'd3-selection'
+const Latex = require('react-latex');
+
+
+class CircleArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      N: 100,
+      inCount: 0,
+      totalCount: 0,
+      squareColor:'#FF1493',
+      circleColor: '#0000FF',
+    };
+  }
+
+  componentDidMount() {
+    this.createSimulation();
+  }
+
+  createSimulation() {
+    // reset svg
+    const node = this.node;
+    select(node).html('');
+
+    const d = Math.min(this.props.height, this.props.width);
+    const r = d / 2 ;
+    const cx = this.props.width / 2;
+    const cy = this.props.height / 2;
+
+    // append circle
+    select(node).append('circle')
+                .attr('cx', cx)
+                .attr('cy', cy)
+                .attr('r', r)
+                .style('stroke', this.state.circleColor)
+                .style('fill', 'none');
+
+    // append square
+    select(node).append('rect')
+                .attr('width', d)
+                .attr('height', d)
+                .attr('x', this.props.width / 2 - r)
+                .attr('y', this.props.height / 2 - r)
+                .style('stroke', this.state.squareColor)
+                .style('fill', 'none');
+  }
+
+  runSimulation(N) {
+    const node = this.node;
+
+    const d = Math.min(this.props.height, this.props.width);
+    const r = d / 2 ;
+    const cx = this.props.width / 2;
+    const cy = this.props.height / 2;
+
+    // function to identify if a point is in the circle
+    this.isIn = function(p) {
+      return Math.sqrt((p.x - cx) * (p.x - cx) + (p.y - cy) * (p.y - cy)) <= r;
+    }
+
+    // function to generate a point uniformly at random
+    this.genPoint = function() {
+        var x = Math.floor(Math.random() * this.props.width);
+        var y = Math.floor(Math.random() * this.props.height);
+        return {x, y};
+    }
+
+    // modifying state would not work due to the async nature of setState
+    var inCount = 0;
+    var totalCount = 0;
+    // create points
+    for (var i = 0; i < N; i++) {
+      let p = this.genPoint();
+
+      // append node
+      setTimeout(() => {
+        select(node).append('circle')
+                    .attr('cx', p.x)
+                    .attr('cy', p.y)
+                    .attr('r', 3)
+                    .style('fill', this.isIn(p) ? this.state.circleColor : this.state.squareColor);
+
+        if (this.isIn(p)) {
+          inCount++;
+        }
+        totalCount++;
+
+        this.setState({
+          inCount,
+          totalCount
+        });
+
+      }, 30);
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const N = Number(document.getElementById('N').value);
+    this.setState({ N });
+    this.createSimulation();
+    this.runSimulation(N);
+  }
+
+  renderDetails = () => {
+    return (
+      <div>
+        <h3>{'Monte Carlo Estimation of Pi using circle\'s area'}</h3>
+          <Latex>
+          Assuming the square has side length , we know that the
+          area of the square is $d^2$ while the area of the circle is $\pi(\frac d 2)^2$ $= \frac 1 4 \pi d^2$
+          The ratio of the area of the circle to the area of the square is equal to $\frac \pi 4$
+          </Latex>
+          <br/>
+          <br/>
+          <Latex>
+          By selecting points uniformly at random from the square, we know that the probability
+          of the point being within the circle is $p=\frac \pi 4$. We can estimate the probability using a Monte Carlo
+          simulation and thus derive an estimation of $\pi$
+          </Latex>
+          <br/>
+          <br/>
+
+          <form className="form-inline" onSubmit={this.handleSubmit}>
+          <label className="sr-only" htmlFor="N">N</label>
+          <div className="input-group mb-2 mr-sm-2">
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+                N
+              </div>
+            </div>
+            <input type="number" className="form-control" id='N' placeholder='100' max="10000"/>
+          </div>
+
+          <button type="submit" className="btn btn-primary mb-2">Submit</button>
+          </form>
+
+          <table className="table">
+            <tbody>
+              <tr>
+                <th scope="row"># of inside points</th>
+                <td>{this.state.inCount}</td>
+              </tr>
+
+              <tr>
+                <th scope="row"># of total points</th>
+                <td>{this.state.totalCount}</td>
+              </tr>
+
+              <tr>
+                <th scope="row"><Latex>$\pi$ Estimate</Latex></th>
+                <td>{4 * this.state.inCount / this.state.totalCount || undefined}</td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+    );
+  }
+
+  render () {
+    return (
+      <div className='row justify-content-md-center' style={{width: 'inherit'}}>
+        <div className='col-lg-7 order-last'>
+          <svg ref={node => this.node = node}
+            width = {this.props.width} height = {this.props.height}>
+          </svg>
+        </div>
+        <div className='col-lg-5 order-first'>
+          {this.renderDetails()}
+        </div>
+      </div>
+    );
+  }
+}
+
+CircleArea.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+}
+
+export default CircleArea;
